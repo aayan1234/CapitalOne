be5286f592acc86b472e1d220568a67c4546770d
arifhussain1234 <34404347+arifhussain1234@users.noreply.github.com>
2018-11-29 23:12:09 -0500

    Hello this is my commit

--- src/main/java/gitpriactice/Demo1.java
+++ src/main/java/gitpriactice/Demo1.java
@@ -4,9 +4,14 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		System.out.println("Learning Git");
-		String s ="Hello arif";
+		String s = "Hello arif";
 		String s1 = "why are you yawning";
 
+		int b = 20;
+		int c = 30;
+		int d = 30;
+		double e = 40.0;
+		char x = 'r';
 	}
 
 }
