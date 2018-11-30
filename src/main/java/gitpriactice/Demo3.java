a7943f402a68cb172116f1ae413990325635b38c
arifhussain1234 <34404347+arifhussain1234@users.noreply.github.com>
2018-11-29 22:52:44 -0500

    added file

---
+++ src/main/java/gitpriactice/Demo3.java
@@ -0,0 +1,10 @@
+package gitpriactice;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/gitpriactice/Demo4.java
@@ -0,0 +1,14 @@
+package gitpriactice;
+
+public class Demo4 {
+
+	int a=10;
+	int b=20;
+	int c =100;
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
